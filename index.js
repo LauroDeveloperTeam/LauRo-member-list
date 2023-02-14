707268f36364a6db411dc87e3375304bc93c3357
yuyutti <yuyuttiyuyutti.com>
2023-02-14 19:53:57 +0900

    flamingo先生によるご教授通りに行いました。

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const SteinStore = require("stein-js-client");
+const store = new SteinStore("https://api.steinhq.com/v1/storages/63eb636aeced9b09e9beb441");
+app.use(express.static('./'));
+app.set('view engine', 'ejs')
+app.listen(3000, function(){
+  console.log('起動完了 => Express')
+})
+app.get('/',(req, res) => {
+  store.read("index", {authentication: { username: "yuyuttikunnhatennsainanodaxa", password: "fwuasicjnks@fecknfaweioknmjsedfvnihowfveomfowe" }}).then(data => {
+    res.render('index',{data:data});
+  });
+})
--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(express.static('./'));
-app.listen(3000, function(){
-  console.log('起動完了 => Express')
-})
